87dadcfeb37674aab93ba3691b4791fa577d2e43
fullstack <david.elbel@fullstackwebdevelopment.com>
2019-01-04 17:40:22 -0800

    Add custom.d.ts and add hook

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { hot } from 'react-hot-loader/root'
+import { useState } from 'react';
+
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+ const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clisdfsd {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <Example />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
